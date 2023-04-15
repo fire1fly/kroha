a66eff4a5dc71094245881dfbb11dbe67297be93
fire1fly <danyseidev@gmail.com>
2023-04-15 16:55:26 +0800

    feat: header full and mobile menu

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -4,23 +4,86 @@ const menuTriggerClass = "h-menubtn";
 const menuEl = document.querySelector(`.${menuClass}`);
 const menuBtnEl = document.querySelector(`.${menuTriggerClass}`);
 
+
+const menuWrapper = document.querySelector(".h");
+let menuWrapperHeight = menuWrapper.clientHeight;
+const menuBackdrop = document.querySelector(".h-backdrop");
+const menuBar = document.documentElement.clientWidth <= 520 ? document.querySelector(".h-socbar") : null;
+
+const elToActive = [menuWrapper, menuEl, menuBackdrop];
+const elToTransition = [menuEl, menuBar];
+const duration = 300;
+
 const breakpoint = menuEl.dataset.menuBreakpoint || "991";
 
 const headerMediaQuery = window.matchMedia(`(max-width: ${breakpoint}px)`);
 
+function show(el) {
+  if (el.classList.contains('collapsing')) {
+    return;
+  }
+  el.style.display = 'flex';
+  el.style.opacity = 0;
+  el.style.transition = `opacity ${duration}ms ease-in-out`;
+  el.classList.add('collapsing');
+  window.setTimeout(() => {
+    el.style.opacity = 1;
+  }, 0);
+  window.setTimeout(() => {
+    el.classList.remove('collapsing');
+    // el.classList.remove('accordion__item_slidedown');
+    el.classList.add('show');
+    el.style.display = '';
+    el.style.opacity = '';
+    el.style.transition = '';
+  }, duration);
+}
+
+function hide(el) {
+  if (el.classList.contains('collapsing')) {
+    return;
+  }
+  el.style.opacity = 1;
+  el.style.display = 'flex';
+  el.style.opacity = 0;
+  el.style.transition = `opacity ${duration}ms ease-in-out`;
+  // el.classList.remove('collapse');
+  el.classList.remove('show');
+  el.classList.add('collapsing');
+  window.setTimeout(() => {
+    el.classList.remove('collapsing');
+    // el.classList.add('collapse');
+    el.style.display = '';
+    el.style.opacity = '';
+    el.style.transition = '';
+  }, duration);
+}
 
 function toggleMenu() {
-  menuBtnEl.classList.toggle("active");
-  menuEl.classList.toggle("active");
+
+  if (menuEl.classList.contains("active")) {
+    menuBtnEl.classList.remove("active");
+    elToActive.forEach(el => el.classList.remove("active"));
+    elToTransition.forEach(el => hide(el));
+  } else {
+    menuWrapper.style.height = `${menuWrapperHeight}px`;
+    menuBtnEl.classList.add("active");
+    elToActive.forEach(el => el.classList.add("active"));
+    elToTransition.forEach(el => show(el));
+
+  }
+
 }
 
 function handleOutsideClick(e) {
   const menu = e.target.closest(`.${menuClass}`);
   const btn = e.target.closest(`.${menuTriggerClass}`);
+  const bar = e.target.closest('.h-socbar')
 
-  if (!(menu || btn)) {
+  if (!(menu || btn || bar)) {
     menuBtnEl.classList.remove("active");
     menuEl.classList.remove("active");
+    elToActive.forEach(el => el.classList.remove("active"));
   }
 }
 
