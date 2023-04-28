b84282b7a30a573dabc32e9f0b39b369eae71bab
fire1fly <danyseidev@gmail.com>
2023-04-28 18:47:30 +0800

    feat: registration form component

--- js/temp_form.js
+++ js/temp_form.js
@@ -1,10 +1,10 @@
 // временный скрипт для того чтобы показать как работают лейблы и прочие элементы полей и формы
 
-const fields = document.querySelectorAll(".input2-field");
+const fields = document.querySelectorAll(".input-field, .input2-field");
 
 fields.forEach(field => {
-  const input = field.querySelector(".input2")
-  const label = field.querySelector(".input2-label")
+  const input = field.querySelector(".input, .input2")
+  const label = field.querySelector(".input-label, .input2-label")
   input.addEventListener("focus", () => field.classList.add("focus"));
   input.addEventListener("blur", e => {
     if (e.target.value === '') {
